3644fafc65a7622afa4543b6c2e891f96922d126
KimJunYeop <1025jy@naver.com>
2023-11-17 17:53:27 +0900

    202311171753

---
+++ BackEnd/src/main/java/com/ssafy/back/member/model/service/MemberServiceImpl.java
@@ -0,0 +1,53 @@
+package com.ssafy.back.member.model.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.ssafy.back.member.model.MemberDto;
+import com.ssafy.back.member.model.mapper.MemberMapper;
+
+@Service
+public class MemberServiceImpl implements MemberService {
+
+	private MemberMapper memberMapper;
+	
+	public MemberServiceImpl(MemberMapper memberMapper) {
+		super();
+		this.memberMapper = memberMapper;
+	}
+	
+	
+	@Override
+	public MemberDto login(MemberDto memberDto) throws Exception {
+		return memberMapper.login(memberDto);
+	}
+
+	@Override
+	public MemberDto userInfo(String userId) throws Exception {
+		return memberMapper.userInfo(userId);
+	}
+
+	@Override
+	public void saveRefreshToken(String userId, String refreshToken) throws Exception {
+		Map<String, String> map =new HashMap<String, String>();
+		map.put("userId", userId);
+		map.put("token", refreshToken);
+		memberMapper.saveRefreshToken(map);
+	}
+
+	@Override
+	public Object getRefreshToken(String userId) throws Exception {
+		return memberMapper.getRefreshToken(userId);
+	}
+
+	@Override
+	public void deleteRefreshToken(String userId) throws Exception {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("userId", userId);
+		map.put("token", null);
+		memberMapper.deleteRefreshToken(map);
+	}
+
+}
